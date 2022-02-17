76891733edd11df0c778f0a3acbc1afb2ca8f045
Mateus Rizzo <mateusrizzodev@gmail.com>
2022-02-17 16:41:21 -0300

    adicionado botao de editar com icone

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+
+import edit from '../../assets/icons/edit.png';
 
 import styles from './styles';
 
 export default function Header() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.headerText}>Suas Finanças</Text>
+      <TouchableOpacity style={styles.editButton}>
+        <Image style={styles.editIcon} source={edit} />
+      </TouchableOpacity>
     </View>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,14 +1,29 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  headerText: {
-    fontFamily: 'Poppins-Regular',
-    color: '#FFF',
-    fontSize: 22,
+  container: {
     paddingLeft: 30,
     paddingTop: 10,
     paddingBottom: 10,
     backgroundColor: '#105D38',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontFamily: 'Poppins-Regular',
+    color: '#FFF',
+    fontSize: 22,
+  },
+  editButton: {
+    width: 42,
+    height: 42,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  editIcon: {
+    width: 24,
+    height: 24,
   },
 });
 
