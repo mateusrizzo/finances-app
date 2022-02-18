6bbbc357e6fdc9b93becc4316f520b4481d026fe
Mateus Rizzo <mateusrizzodev@gmail.com>
2022-02-18 11:54:40 -0300

    Adicionada sombra no fab

--- src/components/FAB/styles.js
+++ src/components/FAB/styles.js
@@ -11,6 +11,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     bottom: 30,
     right: 30,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
   plusIcon: {
     fontFamily: 'Poppins-Regular',
